d90e759bbe63580d30d91978a0c36a5c9ab49cff
maxth <maxthalehim@icloud.com>
2024-09-14 20:56:34 +0200

    minecrfat works t now

--- src/main/kotlin/hello/PluginTemplate.kt
+++ src/main/kotlin/hello/PluginTemplate.kt
@@ -1,15 +1,22 @@
 package hello
 
+
+import hello.SpringApplication
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import org.springframework.boot.runApplication
 
 class PluginTemplate : JavaPlugin() {
 
 
-
     override fun onEnable() {
-    }
-
-    override fun onDisable() {
-
+        Bukkit.getServer().consoleSender.sendMessage("§6Sergrerever AKTIV")
+        Bukkit.getServer().consoleSender.sendMessage("§6Server AKTIV")
+        Bukkit.getServer().consoleSender.sendMessage("§6Server AKTIV OLIVER")
+        Bukkit.getServer().consoleSender.sendMessage("§6Server AKTIV")
+        Bukkit.getServer().consoleSender.sendMessage("§6Server AKTIV")
+        Bukkit.getServer().consoleSender.sendMessage("§6Server AKTIV")
+        Bukkit.getServer().consoleSender.sendMessage("§6Server AKTIV 2024!!!!XD")
+        runApplication<SpringApplication>()
     }
 }
